5232d7b09060e4ca6b66b8a923a2a1a5ecf7e834
Abhin Pai <abhinpai96@gmail.com>
2021-07-03 22:46:39 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h2>hello</h2>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
